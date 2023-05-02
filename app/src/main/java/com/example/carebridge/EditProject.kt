97d02a8f219b59e3ad405dbe5fdb3f87d509a35d
Poorna Theekshana <poornatheekshana@outlook.com>
2023-05-02 23:01:42 +0530

    Create Add Project

--- app/src/main/java/com/example/carebridge/EditProject.kt
+++ app/src/main/java/com/example/carebridge/EditProject.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Educationfragment : Fragment() {
+class EditProject : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_educationfragment, container, false)
+        return inflater.inflate(R.layout.fragment_edit_project, container, false)
     }
 }
