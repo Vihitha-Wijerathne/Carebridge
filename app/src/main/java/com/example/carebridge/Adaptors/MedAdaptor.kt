f38bf2b2b6f5fd7f9fc70928c10bc64e1bdae8ee
Binara Prabhanga <binaraprabhanga@gmail.com>
2023-05-04 00:35:27 +0530

    Medicine view commit

---
+++ app/src/main/java/com/example/carebridge/Adaptors/MedAdaptor.kt
@@ -0,0 +1,55 @@
+package com.example.carebridge.Adaptors
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.carebridge.Models.ProjectModel
+import com.example.carebridge.R
+
+
+class MedAdaptor (private val projectList: ArrayList<ProjectModel>) : RecyclerView.Adapter<MedAdaptor.ViewHolder>() {
+
+    private lateinit var mListner: onItemClickListner
+
+    interface onItemClickListner{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListner(clickListner: onItemClickListner){
+        mListner = clickListner
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.project_list_view, parent,false)
+        return ViewHolder(itemView, mListner)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = projectList[position]
+        if(currentItem.projecType=="Medicine") {
+            holder.projectName.text = currentItem.projectName
+            holder.projectDescription.text = currentItem.projectDescription
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return projectList.size
+    }
+
+    class ViewHolder(itemView: View, clickListner: onItemClickListner) : RecyclerView.ViewHolder(itemView) {
+
+        val projectName : TextView = itemView.findViewById(R.id.educardtopic)
+        val projectDescription : TextView = itemView.findViewById(R.id.educard_description)
+
+        init {
+            itemView.setOnClickListener{
+                clickListner.onItemClick(adapterPosition)
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/carebridge/Medicinefragment.kt
+++ app/src/main/java/com/example/carebridge/Medicinefragment.kt
@@ -1,23 +1,92 @@
 package com.example.carebridge
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.carebridge.Adaptors.MedAdaptor
+import com.example.carebridge.Models.ProjectModel
+import com.google.firebase.database.*
 
 
 class Medicinefragment : Fragment() {
 
-
+    private lateinit var projectsRecyclerView: RecyclerView
+    private lateinit var loadingData: TextView
+    private lateinit var projectList: ArrayList<ProjectModel>
+    private lateinit var dbRef: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medicinefragment, container, false)
+        val view = inflater.inflate(R.layout.fragment_medicinefragment, container, false)
+
+        projectsRecyclerView = view.findViewById(R.id.medicineprojects)
+        projectsRecyclerView.layoutManager = LinearLayoutManager(context)
+        projectsRecyclerView.setHasFixedSize(true)
+        loadingData = view.findViewById(R.id.medicineLoading)
+
+        projectList = arrayListOf<ProjectModel>()
+
+        getProjectData()
+
+
+        return view
     }
 
+    private fun getProjectData(){
+        projectsRecyclerView.visibility = View.GONE
+        loadingData.visibility = View.VISIBLE
 
+        dbRef = FirebaseDatabase.getInstance().getReference("Project")
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                projectList.clear()
+                if (snapshot.exists()){
+                    for (projectSnap in snapshot.children){
+                        val projectData = projectSnap.getValue(ProjectModel::class.java)
+                        projectList.add(projectData!!)
+                    }
+                    val projectAdaptor = MedAdaptor(projectList)
+                    projectsRecyclerView.adapter = projectAdaptor
+
+                    projectAdaptor.setOnItemClickListner(object : MedAdaptor.onItemClickListner {
+                        override fun onItemClick(position: Int) {
+
+                            val intent = Intent(context, MedicineCard::class.java)
+
+                            //put extras
+                            intent.putExtra("pId", projectList[position].projectId)
+                            intent.putExtra("pName", projectList[position].projectName)
+                            intent.putExtra("pDate", projectList[position].projectDate)
+                            intent.putExtra("pTime", projectList[position].projectTime)
+                            intent.putExtra("pLocation", projectList[position].projectLocation)
+                            intent.putExtra("pDescription", projectList[position].projectDescription)
+                            intent.putExtra("pContact", projectList[position].projectContact)
+                            startActivity(intent)
+
+
+                        }
+
+                    })
+
+                    projectsRecyclerView.visibility = View.VISIBLE
+                    loadingData.visibility = View.GONE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
 }
