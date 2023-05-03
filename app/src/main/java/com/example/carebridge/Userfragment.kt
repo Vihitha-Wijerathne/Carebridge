5a55134085a94ff32b3222ed5c2bd0b6fd647b97
Poorna Theekshana <poornatheekshana@outlook.com>
2023-05-03 10:15:12 +0530

    Connect Add Edit Pages

--- app/src/main/java/com/example/carebridge/Userfragment.kt
+++ app/src/main/java/com/example/carebridge/Userfragment.kt
@@ -1,10 +1,12 @@
 package com.example.carebridge
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class Userfragment : Fragment() {
 
@@ -14,12 +16,23 @@ class Userfragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_userfragment, container, false)
+        val view = inflater.inflate(R.layout.fragment_userfragment, container, false)
 
+        val addProjectBtn = view.findViewById<Button>(R.id.addprojectbtn)
+        addProjectBtn.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView2, AddProject())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        val viewProjectBtn = view.findViewById<Button>(R.id.viewprojectsbtn)
+        viewProjectBtn.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView2, EditProject())
+                .addToBackStack(null)
+                .commit()
+        }
+        return view
     }
-
-
-
-
-
 }
