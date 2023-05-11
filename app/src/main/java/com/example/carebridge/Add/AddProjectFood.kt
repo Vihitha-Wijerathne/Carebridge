636e589478b8ff9adcc33818b695a8a4320f8ab7
Poorna Theekshana <poornatheekshana@outlook.com>
2023-05-11 16:01:10 +0530

    Time Picker Changes

--- app/src/main/java/com/example/carebridge/Add/AddProjectFood.kt
+++ app/src/main/java/com/example/carebridge/Add/AddProjectFood.kt
@@ -2,11 +2,11 @@ package com.example.carebridge.Add
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.fragment.app.Fragment
 import com.example.carebridge.Models.ProjectModel
 import com.example.carebridge.R
 import com.example.carebridge.UserMainFrag.SelectProjectType
